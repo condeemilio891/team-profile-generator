40de5f57b8530b911a6494adec8b123a57ad00fe
Emilio Conde <condeemilio891@gmail.com>
2020-09-29 19:52:51 -0500

    added some inquirer prompts

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -15,18 +15,58 @@ const Employee = require("./lib/Employee");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-var profile=new Promise(function(resolve,reject){
-    resolve(inquirer.prompt(
-        {type: "rawlist",
-        name:'Etype',
-        message:"What is your role?",
-        choices:[Employee,Manager,Intern,Engineer]}
-    ))
-})
 
-// profile.then(function(data){
-//     fs.writeFile
-// })
+function employeePrompt(){
+    return inquirer.prompt([
+            {type:'input',
+            name:'name',
+            message:"what is your name"},{
+                type:'input',
+                message:'What is your ID number?',
+                name:"id",},{
+                    type:'input',
+                    message:'what is your Email address?'
+                    ,name:'email'
+                },{
+                    type:'input',
+                    message:'what it you role',
+                    name:'role',
+                },
+        ],)
+                };
+
+
+
+
+                inquirer
+                .prompt([
+                    {type:'input',
+                    name:'name',
+                    message:"what is your name"},{
+                        type:'input',
+                        message:'What is your ID number?',
+                        name:"id",},{
+                            type:'input',
+                            message:'what is your Email address?'
+                            ,name:'email'
+                        },{
+                            type:'input',
+                            message:'what it you role',
+                            name:'role',
+                        },
+                ],)
+                        
+                .then(() => {
+                  inquirer.prompt({
+                    type: 'list',
+                    name: 'beverage',
+                    message: 'And your favorite beverage?',
+                    choices: ['Pepsi', 'Coke', '7up', 'Mountain Dew', 'Red Bull'],
+                  });
+                });
+
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
